5cd25ee679de75f845821e598b5c14166cbc8abd
Simon Seyer <simon.seyer@cobi.bike>
2016-04-24 16:53:52 +0200

    Showed image location on map

--- Story/TripViewController.swift
+++ Story/TripViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class TripViewController: UIViewController {
+class TripViewController: UIViewController, UIPageViewControllerDelegate {
     
     var tripView: TripView?
+    let model: Trip
     
     let pageViewController: TripPageViewController
     
     init(model: Trip) {
+        self.model = model
         pageViewController = TripPageViewController(model: model)
         super.init(nibName: nil, bundle: nil)
     }
@@ -34,6 +38,48 @@ class TripViewController: UIViewController {
         addChildViewController(pageViewController)
         pageViewController.didMoveToParentViewController(self)
         tripView?.setDayView(pageViewController.view)
+        
+        pageViewController.delegate = self
+        
+        addMarkers()
+        if let day = model.days.first {
+            centerMapView(day, animated: false)
+        }
     }
     
+    private func addMarkers() {
+        for day in model.days {
+            tripView?.mapView.addAnnotation(DayAnnotation(day: day))
+        }
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let viewControllers = pageViewController.viewControllers, dayViewController = viewControllers[0] as? DayViewController {
+            centerMapView(dayViewController.day, animated: true)
+        }
+    }
+    
+    private func centerMapView(dayModel: Day, animated: Bool) {
+        let day = DayAnnotation(day: dayModel)
+        if let mapView = tripView?.mapView {
+            let viewRegion = MKCoordinateRegionMakeWithDistance(day.coordinate, 500, 500)
+            let region = mapView.regionThatFits(viewRegion)
+            mapView.setRegion(region, animated: animated)
+        }
+    }
+    
+}
+
+
+@objc class DayAnnotation : NSObject, MKAnnotation {
+    
+    let day: Day
+    
+    init(day: Day) {
+        self.day = day
+    }
+    
+    internal  var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(day.image.latitude, day.image.longitude)
+    }
 }
