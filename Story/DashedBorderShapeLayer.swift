462b40a4f6c380e5e5efa9650c782ddb3418f271
Simon Seyer <simon.seyer@cobi.bike>
2016-04-25 23:33:52 +0200

    Implemented DashedBorderShapeLayer

---
+++ Story/DashedBorderShapeLayer.swift
@@ -0,0 +1,40 @@
+//
+//  DashedBorderShapeLayer.swift
+//  Story
+//
+//  Created by COBI on 25.04.16.
+//
+//
+
+import UIKit
+
+class DashedBorderShapeLayer : CAShapeLayer {
+    
+    override init(layer: AnyObject) {
+        super.init(layer: layer)
+        setupLayer()
+    }
+    
+    override init() {
+        super.init()
+        setupLayer()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    private func setupLayer() {
+        strokeColor = UIColor(hexValue: ViewConstants.borderColorCode).CGColor
+        fillColor = nil
+        lineDashPattern = [6, 5]
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            path = UIBezierPath(roundedRect: frame, cornerRadius: 8).CGPath
+        }
+    }
+    
+}
